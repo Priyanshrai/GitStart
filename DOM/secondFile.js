85d8f5044cc09e5b3a41d9e2708f33e9a0b410d3
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-09 21:44:15 +0530

    Added item3 color green and all item bold

--- DOM/secondFile.js
+++ DOM/secondFile.js
@@ -11,3 +11,9 @@ var addItem=document.getElementsByClassName('title');
 console.log(addItem[0])
 addItem[0].style.fontWeight='bold';
 addItem[0].style.color='green';
+
+var item=document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor='green';
+for(i=0;i<item.length;i++){
+    item[i].style.fontWeight='bold';
+}
