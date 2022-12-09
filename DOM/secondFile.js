e590a2907bcbbe8c75b133ba3f0f9f8adc119593
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-09 22:05:00 +0530

    added by tagname

--- DOM/secondFile.js
+++ DOM/secondFile.js
@@ -1,19 +1,20 @@
-//Get element by id
+// //Get element by id
 
-var headerTitle=document.getElementById('header-title');
-headerTitle.textContent='Hello';
-headerTitle.innerText='GoodBye';
-console.log(headerTitle.textContent)
-headerTitle.innerHTML = '<h3> 10245 </h3>';
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
-var addItem=document.getElementsByClassName('title');
-console.log(addItem[0])
-addItem[0].style.fontWeight='bold';
-addItem[0].style.color='green';
+// var headerTitle=document.getElementById('header-title');
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+// console.log(headerTitle.textContent)
+// headerTitle.innerHTML = '<h3> 10245 </h3>';
+// var header=document.getElementById('main-header');
+// header.style.borderBottom='solid 3px #000';
+// var addItem=document.getElementsByClassName('title');
+// console.log(addItem[0])
+// addItem[0].style.fontWeight='bold';
+// addItem[0].style.color='green';
 
 var item=document.getElementsByClassName('list-group-item');
-item[2].style.backgroundColor='green';
-for(i=0;i<item.length;i++){
-    item[i].style.fontWeight='bold';
-}
+item[3].style.backgroundColor='green';
+console.log(item[3])
+var li=document.getElementsByTagName('li');
+li[4].style.fontWeight='bold';
+
