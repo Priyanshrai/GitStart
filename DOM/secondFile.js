4607fc41718e6a2a24aa7ead12ec5d54e9f20a24
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-09 22:25:29 +0530

    added querySelector

--- DOM/secondFile.js
+++ DOM/secondFile.js
@@ -12,9 +12,21 @@
 // addItem[0].style.fontWeight='bold';
 // addItem[0].style.color='green';
 
-var item=document.getElementsByClassName('list-group-item');
-item[3].style.backgroundColor='green';
-console.log(item[3])
-var li=document.getElementsByTagName('li');
-li[4].style.fontWeight='bold';
+// var item=document.getElementsByClassName('list-group-item');
+// item[3].style.backgroundColor='green';
+// console.log(item[3])
+// var li=document.getElementsByTagName('li');
+// li[4].style.fontWeight='bold';
 
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = "hidden";
+
+var li = document.querySelectorAll('li');
+li[1].style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
