6bf5cb6a0a57f811672bb23dcf68f120be5934c6
Priyanshrai <rai.priyansh007@gmail.com>
2022-12-09 20:35:54 +0530

    new

--- DOM/secondFile.js
+++ DOM/secondFile.js
@@ -4,7 +4,7 @@ var headerTitle=document.getElementById('header-title');
 headerTitle.textContent='Hello';
 headerTitle.innerText='GoodBye';
 console.log(headerTitle.textContent)
-headerTitle.innerHTML = '<h3> Fuck </h3>';
+headerTitle.innerHTML = '<h3> 10245 </h3>';
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
 var addItem=document.getElementsByClassName('title');
